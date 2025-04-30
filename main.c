c1e53855575a534f017cbf986e785afc58a864a6
Proxy539 <roman.filiyutich@gmail.com>
2025-04-30 20:59:33 +0300

    make init template

--- main.c
+++ main.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 
+    FILE *fp = fopen("text.txt", "r");
 
-    printf("Hello, TextAnalyzer app!\n");
+    if (!fp) {
+        fprintf(stderr, "Can't open file text.txt\n");
+        exit(EXIT_FAILURE);
+    }
+
+
+    printf("Reading file text.txt\n");
+
+
+    
+
+
+    if (!fclose(fp)) {
+        printf("File text.txt closed successfully\n");
+    } else {
+        printf("Can't close file text.txt\n");
+    }
+    
 
     return 0;
 }
